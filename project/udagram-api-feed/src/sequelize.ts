24aeade5207c9d88d735443a463453da91c0f07c
Terry Wong <terrywon@cisco.com>
2021-03-15 15:48:28 -0700

    Fixed DB URI

--- project/udagram-api-feed/src/sequelize.ts
+++ project/udagram-api-feed/src/sequelize.ts
@@ -3,12 +3,11 @@ import {config} from './config/config';
 
 
 export const sequelize = new Sequelize({
-  url: `postgresql://${config.username}:${config.password}@${config.host}/${config.database}`,
-  // username: config.username,
-  // password: config.password,
-  // database: config.database,
-  // host: config.host,
-  //
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+
   dialect: config.dialect,
   storage: ':memory:',
 });
